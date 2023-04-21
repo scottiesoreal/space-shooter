103b00b0e7e100feb412a206d187db545c8077f2
Sourile <snsourile@utep.edu>
2023-04-21 16:17:09 -0600

    enemy prefab with random movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4.0f;
+    private GameObject _Enemy;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 6f, 0);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move down 4 meters per second
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        
+        //if bottom of screen
+        if (transform.position.y <= -10.55f)
+        {
+            //respawn at top with a new random x Position
+            transform.position = new Vector3(Random.Range(-9.50f, 9.50f), 7.60f, 0);
+        }
+        
+
+    }
+}
