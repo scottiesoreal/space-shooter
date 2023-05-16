a619c1b742530504dfcd87331d96315f26b0d60e
scottiesoreal <83109328+scottiesoreal@users.noreply.github.com>
2023-05-16 14:36:56 -0600

    Asteroid explosion; waiting for assistance

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotationSpeed = 3.0f;    
+    [SerializeField]
+    private GameObject _explosionPrefab;
+
+
+   
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * _rotationSpeed * Time.deltaTime);
+    }
+
+    //check for LASER collision (Trigger)
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       
+        if (other.tag == "Laser")
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    //isnantiate explosion at the position of the asteroid
+    //destroy the explosion after 3 seconds
+
+}
